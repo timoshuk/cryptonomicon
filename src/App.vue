f37fb1bb88a205fd11af7b6bece7511de05102ff
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-09-22 16:57:36 +0300

    add ugly project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,236 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <section>
+      <div class="flex">
+        <div class="max-w-xs">
+          <label for="wallet" class="block text-sm font-medium text-gray-700"
+            >Тикер</label
+          >
+          <div class="mt-1 relative rounded-md shadow-md">
+            <input
+              v-model="ticker"
+              @keydown.enter="addTicket"
+              type="text"
+              name="wallet"
+              id="wallet"
+              class="
+                block
+                w-full
+                pr-10
+                border-gray-300
+                text-gray-900
+                focus:outline-none focus:ring-gray-500 focus:border-gray-500
+                sm:text-sm
+                rounded-md
+              "
+              placeholder="Например DOGE"
+            />
+          </div>
+          <div
+            class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+          >
+            <span
+              class="
+                inline-flex
+                items-center
+                px-2
+                m-1
+                rounded-md
+                text-xs
+                font-medium
+                bg-gray-300
+                text-gray-800
+                cursor-pointer
+              "
+            >
+              BTC
+            </span>
+            <span
+              class="
+                inline-flex
+                items-center
+                px-2
+                m-1
+                rounded-md
+                text-xs
+                font-medium
+                bg-gray-300
+                text-gray-800
+                cursor-pointer
+              "
+            >
+              DOGE
+            </span>
+            <span
+              class="
+                inline-flex
+                items-center
+                px-2
+                m-1
+                rounded-md
+                text-xs
+                font-medium
+                bg-gray-300
+                text-gray-800
+                cursor-pointer
+              "
+            >
+              BCH
+            </span>
+            <span
+              class="
+                inline-flex
+                items-center
+                px-2
+                m-1
+                rounded-md
+                text-xs
+                font-medium
+                bg-gray-300
+                text-gray-800
+                cursor-pointer
+              "
+            >
+              CHD
+            </span>
+          </div>
+          <div v-if="false" class="text-sm text-red-600">
+            Такой тикер уже добавлен
+          </div>
+        </div>
+      </div>
+      <button
+        @click="addTicket"
+        type="button"
+        class="
+          my-4
+          inline-flex
+          items-center
+          py-2
+          px-4
+          border border-transparent
+          shadow-sm
+          text-sm
+          leading-4
+          font-medium
+          rounded-full
+          text-white
+          bg-gray-600
+          hover:bg-gray-700
+          transition-colors
+          duration-300
+          focus:outline-none
+          focus:ring-2
+          focus:ring-offset-2
+          focus:ring-gray-500
+        "
+      >
+        <!-- Heroicon name: solid/mail -->
+        <svg
+          class="-ml-0.5 mr-2 h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="#ffffff"
+        >
+          <path
+            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+          ></path>
+        </svg>
+        Добавить
+      </button>
+    </section>
+    <template v-if="tickers.length">
+      <hr class="w-full border-t border-gray-600 my-4" />
+      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <div
+          v-for="item in tickers"
+          :key="item.id"
+          class="
+            bg-white
+            overflow-hidden
+            shadow
+            rounded-lg
+            border-purple-800 border-solid
+            cursor-pointer
+          "
+        >
+          <div class="px-4 py-5 sm:p-6 text-center">
+            <dt class="text-sm font-medium text-gray-500 truncate">
+              {{ item.name }}
+            </dt>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ item.price }} USD
+            </dd>
+          </div>
+          <div class="w-full border-t border-gray-200"></div>
+          <button
+            @click="deleteItem(item)"
+            class="
+              flex
+              items-center
+              justify-center
+              font-medium
+              w-full
+              bg-gray-100
+              px-4
+              py-4
+              sm:px-6
+              text-md text-gray-500
+              hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
+              transition-all
+              focus:outline-none
+            "
+          >
+            <svg
+              class="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="#718096"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              ></path></svg
+            >Удалить
+          </button>
+        </div>
+      </dl>
+      <hr class="w-full border-t border-gray-600 my-4" />
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      ticker: "",
+      tickers: [
+        { id: 1, name: "WTF", price: "2" },
+        { id: 2, name: "VUE", price: "1.25" },
+        { id: 3, name: "BTC", price: "3.44" },
+      ],
+    };
+  },
+  methods: {
+    addTicket() {
+      this.ticker.length &&
+        this.tickers.push({
+          id: this.tickers.length + 1,
+          name: this.ticker,
+          price: "-",
+        });
+    },
+    deleteItem(deletedItem) {
+      this.tickers = this.tickers.filter((item) => item !== deletedItem);
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./app.css"></style>
